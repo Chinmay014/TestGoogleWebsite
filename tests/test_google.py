6c812bf68ace1fd0f1a7a219079aeedfe563b76b
Chinmay014 <chinmays117@gmail.com>
2024-03-04 22:30:20 +0100

    add testing using json file

--- tests/test_google.py
+++ tests/test_google.py
@@ -3,7 +3,7 @@ from pages.google_launch_page import LaunchPage
 import time, re, pytest
 from pages.google_search_results_page import SearchResultPage
 from Utilities.utils import custLogger
-from ddt import ddt,data,unpack
+from ddt import ddt,data,unpack, file_data
 import unittest 
 
 @pytest.mark.usefixtures("setup")
@@ -13,8 +13,7 @@ class TestGoogleSite(unittest.TestCase):
     log1 = log.customlogger()
     
 
-    @data(("Chocolate",),("Coffee",),("Honung",))
-    @unpack
+    @file_data("../TestData/testdata.json")
     def test_search_results(self,search_query):
         lp = LaunchPage(self.driver)
         search_result = lp.clickSearch(search_query)
@@ -27,8 +26,7 @@ class TestGoogleSite(unittest.TestCase):
         self.log1.warning(f"found {result_count} results for {search_query}")
         assert result_count>0,"No results"
 
-    @data(("Chocolate",),("Tea",))
-    @unpack
+    @file_data("../TestData/testdata.json")
     def test_search_quantity(self,search_query):
         # self.driver = setup
         # test 2: top suggested link contains the searched word atleast 5 times
